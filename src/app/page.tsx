84b267c6e67e9058921b6e2e5cc092f6d7cdc3cb
Paulo Turino <paulo.turino01@gmail.com>
2023-09-01 13:18:47 -0300

    implementado botao para abrir o modal com as regras do jogo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,12 @@ import SelectSection from "@/components/SelectSection";
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center gap-16 p-12">
+    <main className="flex h-screen flex-col items-center gap-16 p-8">
       <Header />
       <SelectSection />
+      <button className="border-2 border-[var(--border-color)] text-[var(--border-color)] absolute z-2 bottom-12 right-12 py-2 px-8 rounded-lg tracking-widest hover:text-[var(--light-font)] hover:border-[var(--light-font)]">
+        REGRAS
+      </button>
     </main>
   )
 }
