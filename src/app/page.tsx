2d3b9b54efecb8dcbc792a78659914c82b8f71af
Paulo Turino <paulo.turino01@gmail.com>
2023-09-01 09:56:36 -0300

    implementando header e redefinindo cores do bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex h-screen flex-col items-center justify-between p-12">
+      <Header />
       Hello World!
     </main>
   )
---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+export default function Header() {
+    return(
+        <header className="border-2 rounded-xl border-[var(--border-color)] w-2/5 py-3 pl-4 pr-3 flex justify-between items-center">
+            <div className="flex flex-col leading-4 text-xl">
+                <p className="text-[var(--light-font)] uppercase">Rock</p>
+                <p className="text-[var(--light-font)] uppercase">Paper</p>
+                <p className="text-[var(--light-font)] uppercase">Scissors</p>
+                <p className="text-[var(--light-font)] uppercase">Lizard</p>
+                <p className="text-[var(--light-font)] uppercase">Spock</p>
+            </div>
+            <div className="bg-[var(--light-font)] rounded-md flex flex-col px-8 py-2 items-center">
+                <p className="text-[var(--blue-font)] text-sm leading-4 tracking-wider">SCORE</p>
+                <p className="text-5xl text-[var(--dark-font)] tracking-wide">12</p>
+            </div>
+        </header>
+    )
+}
