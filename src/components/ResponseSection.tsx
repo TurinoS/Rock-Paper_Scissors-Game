8c3d031aa64da63140979c712f33211e1fa0b7f2
Paulo Turino <paulo.turino01@gmail.com>
2023-09-03 14:07:42 -0300

    adicionada responsividade em toda a aplicação

--- src/components/ResponseSection.tsx
+++ src/components/ResponseSection.tsx
@@ -4,7 +4,6 @@ import rock from "../assets/icon-rock.svg";
 import lizard from "../assets/icon-lizard.svg";
 import spock from "../assets/icon-spock.svg";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 
 interface ResponseProps {
   userPick: number;
@@ -53,7 +52,9 @@ export default function ResponseSection({
   return (
     <>
       {firstStep && (
-        <section>
+        <section className="flex gap-14 md:gap-28"
+            style={{transform: window.innerWidth < 500 ? "scale(0.8)" : "none",}}
+        >
           <div>
             <h3 className="text-xl text-center mb-16 uppercase tracking-widest">
               You picked
@@ -69,6 +70,8 @@ export default function ResponseSection({
                   : "py-7 pr-9 pl-10"
               } rounded-full bg-[var(--light-font)]`}
               style={{
+                width: "186px",
+                height: "186px",
                 border: `20px solid var(--${usercolorChoice}1)`,
                 boxShadow: `inset 0 4px 0 1px rgb(0 0 0 / 0.3), 0 4px 0 1px var(--${usercolorChoice}2)`,
               }}
@@ -93,7 +96,9 @@ export default function ResponseSection({
       )}
 
       {secondStep && (
-        <section className="flex gap-28">
+        <section className="flex gap-14 md:gap-28"
+            style={{transform: window.innerWidth < 500 ? "scale(0.8)" : "none",}}
+        >
           <div>
             <h3 className="text-xl text-center mb-16 uppercase tracking-widest">
               You picked
@@ -109,6 +114,8 @@ export default function ResponseSection({
                   : "py-7 pr-9 pl-10"
               } rounded-full bg-[var(--light-font)]`}
               style={{
+                width: "186px",
+                height: "186px",
                 border: `20px solid var(--${usercolorChoice}1)`,
                 boxShadow: `inset 0 4px 0 1px rgb(0 0 0 / 0.3), 0 4px 0 1px var(--${usercolorChoice}2)`,
               }}
@@ -131,6 +138,8 @@ export default function ResponseSection({
                   : "py-7 pr-9 pl-10"
               } rounded-full bg-[var(--light-font)]`}
               style={{
+                width: "186px",
+                height: "186px",
                 border: `20px solid var(--${housecolorChoice}1)`,
                 boxShadow: `inset 0 4px 0 1px rgb(0 0 0 / 0.3), 0 4px 0 1px var(--${housecolorChoice}2)`,
               }}
@@ -142,58 +151,88 @@ export default function ResponseSection({
       )}
 
       {thirdStep && (
-        <section className="flex gap-16">
-          <div>
-            <h3 className="text-xl text-center mb-16 uppercase tracking-widest">
-              You picked
-            </h3>
-            <p
-              className={`${
-                userPick === 1 || userPick === 2
-                  ? "py-8 pr-9 pl-10"
-                  : userPick === 3
-                  ? "p-9"
-                  : userPick === 4
-                  ? "py-10 pr-9 pl-10"
-                  : "py-7 pr-9 pl-10"
-              } rounded-full bg-[var(--light-font)]`}
-              style={{
-                border: `20px solid var(--${usercolorChoice}1)`,
-                boxShadow: `inset 0 4px 0 1px rgb(0 0 0 / 0.3), 0 4px 0 1px var(--${usercolorChoice}2)`,
-              }}
-            >
-              <Image src={userChoice} alt={userChoice} width={70} />
-            </p>
+        <section className="flex flex-col md:flex-row md:gap-16 gap-8"
+            style={{transform: window.innerWidth < 500 ? "scale(0.8)" : "none",}}
+        >
+          <div className="flex gap-14 md:gap-28">
+            <div>
+              <h3 className="text-xl text-center mb-16 uppercase tracking-widest">
+                You picked
+              </h3>
+              <p
+                className={`${
+                  userPick === 1 || userPick === 2
+                    ? "py-8 pr-9 pl-10"
+                    : userPick === 3
+                    ? "p-9"
+                    : userPick === 4
+                    ? "py-10 pr-9 pl-10"
+                    : "py-7 pr-9 pl-10"
+                } rounded-full bg-[var(--light-font)]`}
+                style={{
+                  width: "186px",
+                  height: "186px",
+                  border: `20px solid var(--${usercolorChoice}1)`,
+                  boxShadow: `inset 0 4px 0 1px rgb(0 0 0 / 0.3), 0 4px 0 1px var(--${usercolorChoice}2)`,
+                }}
+              >
+                <Image src={userChoice} alt={userChoice} width={70} />
+              </p>
+            </div>
+            <div className="self-center mt-16 hidden md:block">
+              <h2 className="text-4xl mb-6 tracking-widest">
+                {wld === "lose"
+                  ? "YOU LOSE"
+                  : wld === "win"
+                  ? "YOU WIN"
+                  : wld === "draw" && "DRAW"}
+              </h2>
+              <button
+                onClick={() => playAgain()}
+                className="bg-[var(--light-font)] rounded-lg py-2 w-full text-[var(--bg1)] tracking-widest"
+              >
+                PLAY AGAIN
+              </button>
+            </div>
+            <div>
+              <h3 className="text-xl text-center mb-16 uppercase tracking-widest">
+                The house picked
+              </h3>
+              <p
+                className={`${
+                  housePick === 1 || housePick === 2
+                    ? "py-8 pr-9 pl-10"
+                    : housePick === 3
+                    ? "p-9"
+                    : housePick === 4
+                    ? "py-10 pr-9 pl-10"
+                    : "py-7 pr-9 pl-10"
+                } rounded-full bg-[var(--light-font)]`}
+                style={{
+                  width: "186px",
+                  height: "186px",
+                  border: `20px solid var(--${housecolorChoice}1)`,
+                  boxShadow: `inset 0 4px 0 1px rgb(0 0 0 / 0.3), 0 4px 0 1px var(--${housecolorChoice}2)`,
+                }}
+              >
+                <Image src={houseChoice} alt={houseChoice} width={70} />
+              </p>
+            </div>
           </div>
-          <div className="self-center mt-16">
-            <h2 className="text-4xl mb-6 tracking-widest">
-                {wld === "lose" ?
-                "YOU LOSE" : wld === "win" ? 
-                "YOU WIN" : wld === "draw" && "DRAW"}
+          <div className="self-center mt-8 md:hidden">
+            <h2 className="text-6xl mb-6 tracking-widest">
+              {wld === "lose"
+                ? "YOU LOSE"
+                : wld === "win"
+                ? "YOU WIN"
+                : wld === "draw" && "DRAW"}
             </h2>
-            <button onClick={() => playAgain()} className="bg-[var(--light-font)] rounded-lg py-2 w-full text-[var(--bg1)] tracking-widest">PLAY AGAIN</button>
-          </div>
-          <div>
-            <h3 className="text-xl text-center mb-16 uppercase tracking-widest">
-              The house picked
-            </h3>
-            <p
-              className={`${
-                housePick === 1 || housePick === 2
-                  ? "py-8 pr-9 pl-10"
-                  : housePick === 3
-                  ? "p-9"
-                  : housePick === 4
-                  ? "py-10 pr-9 pl-10"
-                  : "py-7 pr-9 pl-10"
-              } rounded-full bg-[var(--light-font)]`}
-              style={{
-                border: `20px solid var(--${housecolorChoice}1)`,
-                boxShadow: `inset 0 4px 0 1px rgb(0 0 0 / 0.3), 0 4px 0 1px var(--${housecolorChoice}2)`,
-              }}
+            <button
+              onClick={() => playAgain()}
+              className="bg-[var(--light-font)] rounded-lg py-2 w-full text-[var(--bg1)] tracking-widest"
             >
-              <Image src={houseChoice} alt={houseChoice} width={70} />
-            </p>
+              PLAY AGAIN
+            </button>
           </div>
         </section>
       )}
